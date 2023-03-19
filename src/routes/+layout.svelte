e265623f5886cb8548125841787b63f2f0e16ac6
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-03-19 13:07:10 +0300

    Move global CSS to app.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
 <main>
 	<slot />
 </main>
 
 <style>
-	:global(body) {
-		margin: 0;
-		width: 100vw;
-		height: 100vh;
-
-		overflow-x: hidden;
-		overflow-y: auto;
-	}
-
 	main {
 		width: calc(100% - 40px);
 		height: calc(100% - 48px);
