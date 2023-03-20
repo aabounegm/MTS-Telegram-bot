1fab3d3a004f4913d1226e6791eb7a1663367033
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-03-20 13:18:08 +0300

    Move sample charities to the relevant file

--- src/lib/api/charity.ts
+++ src/lib/api/charity.ts
@@ -15,5 +15,24 @@ export type TCharity = {
 	programs: TCharityProgram[];
 };
 
-export const FUNDS_ASSETS_BASE =
-	'https://test.mospay.mos.ru/mospaynew/newportal/assets/images/charity/';
+export const API_BASE = 'https://test.mospay.mos.ru/mospaynew';
+export const FUNDS_ASSETS_BASE = API_BASE + '/newportal/assets/images/charity/';
+
+export const sampleCharities: TCharity[] = [
+	{
+		code: 'ADULTS0001',
+		name: 'Взрослые',
+		programs: [
+			{
+				serviceName: 'Наука, образование и технологии',
+				serviceCode: 'AAAA99341800000000001',
+				fundName: 'Фонд поддержки слепоглухих «Со-единение»',
+				serviceDescription:
+					'Обеспечение нового качества жизни и реабилитации слепоглухих путем развития инновационных отечественных ассистивных технологий и проведения перспективных исследований в области комплексной социализации инвалидов с нарушенным слухом и зрением',
+				fundUrl: 'https://so-edinenie.org/o-fonde/programmi/nauka-obrazovanie-i-tehnologii',
+				fundLogo: FUNDS_ASSETS_BASE + encodeURIComponent('logo color-1@2x.png'),
+				minimum: 1,
+			},
+		],
+	},
+];
