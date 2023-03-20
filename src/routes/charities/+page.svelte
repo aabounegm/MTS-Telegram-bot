3b0c4622d5b671b741dabbe35cc5405379694791
GuVictory <gubanova.vi10@gmail.com>
2023-03-20 16:34:12 +0300

    feat: rebase to new version of charity page

--- src/routes/charities/+page.svelte
+++ src/routes/charities/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import CopyIcon from '$lib/icons/Copy.svelte';
-	import { browser } from '$app/environment';
 
 	export let data: PageData;
 
