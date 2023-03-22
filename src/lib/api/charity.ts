ec4583d59d91e6c99baf1ab3e81eacad7e51e50d
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-03-23 00:45:24 +0300

    Fix character in URL search parameter
    The '+' was supposed to indicate a space

--- src/lib/api/charity.ts
+++ src/lib/api/charity.ts
@@ -20,7 +20,7 @@ export const FUNDS_ASSETS_BASE = API_BASE + '/newportal/assets/images/charity/';
 export const getAccessTokenUrl = (redirectUrl: string) => {
 	const AUTH_URL_BASE = 'https://login.mos.ru/sps/oauth/ae';
 	const params = new URLSearchParams({
-		scope: 'openid+profile',
+		scope: 'openid profile',
 		response_type: 'code',
 		redirect_uri: 'https://test.mospay.mos.ru/sudir/auth',
 		client_id: 'test.mospay.mos.ru',
