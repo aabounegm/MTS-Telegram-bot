8a8b643eaf1df59d0eedafa9cefe80614d672399
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-03-23 00:08:11 +0300

    Implement access token authentication flow

--- src/lib/api/charity.ts
+++ src/lib/api/charity.ts
@@ -17,6 +17,17 @@ export type TCharity = {
 
 export const API_BASE = 'https://test.mospay.mos.ru/mospaynew';
 export const FUNDS_ASSETS_BASE = API_BASE + '/newportal/assets/images/charity/';
+export const getAccessTokenUrl = (redirectUrl: string) => {
+	const AUTH_URL_BASE = 'https://login.mos.ru/sps/oauth/ae';
+	const params = new URLSearchParams({
+		scope: 'openid+profile',
+		response_type: 'code',
+		redirect_uri: 'https://test.mospay.mos.ru/sudir/auth',
+		client_id: 'test.mospay.mos.ru',
+		state: redirectUrl,
+	});
+	return AUTH_URL_BASE + '?' + params.toString();
+};
 
 export const sampleCharities: TCharity[] = [
 	{
--- src/routes/charities/+layout.server.ts
+++ src/routes/charities/+layout.server.ts
@@ -1,9 +1,24 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
-import { API_BASE, sampleCharities, type TCharity } from '$lib/api/charity';
+import { API_BASE, getAccessTokenUrl, sampleCharities, type TCharity } from '$lib/api/charity';
 
-export const load: LayoutServerLoad = async ({ fetch }) => {
-	const res = await fetch(API_BASE + '/rnip2/charges/charitycatalog');
+export const load: LayoutServerLoad = async ({ fetch, cookies, url }) => {
+	let accessToken = url.searchParams.get('accessToken') ?? cookies.get('accessToken');
+	if (accessToken == undefined) {
+		// No access token found. Go to the MTS login page, and redirect to the same page again
+		throw redirect(307, getAccessTokenUrl(url.href));
+	}
+	const res = await fetch(API_BASE + '/rnip2/charges/charitycatalog', {
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
 	if (!res.ok) {
+		if (res.status === 401) {
+			// Token expired. Redirect to MTS login to renew
+			throw redirect(307, getAccessTokenUrl(url.href));
+		}
+		// Otherwise (some other error)
 		console.error(await res.text());
 		return {
 			charities: sampleCharities,
