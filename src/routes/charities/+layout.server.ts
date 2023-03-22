48254f19d57aa7eae355cd5c0c34a6c8dd781460
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-03-23 00:45:41 +0300

    Fix redirect URL protocol during dev

--- src/routes/charities/+layout.server.ts
+++ src/routes/charities/+layout.server.ts
@@ -6,7 +6,9 @@ export const load: LayoutServerLoad = async ({ fetch, cookies, url }) => {
 	let accessToken = url.searchParams.get('accessToken') ?? cookies.get('accessToken');
 	if (accessToken == undefined) {
 		// No access token found. Go to the MTS login page, and redirect to the same page again
-		throw redirect(307, getAccessTokenUrl(url.href));
+		// SvelteKit seems to default it to HTTP during dev
+		const redirectUrl = getAccessTokenUrl(url.href.replace('http:', 'https:'));
+		throw redirect(307, redirectUrl);
 	}
 	const res = await fetch(API_BASE + '/rnip2/charges/charitycatalog', {
 		headers: {
