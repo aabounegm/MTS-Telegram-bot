3dc746b023fe2364f401da63880bcb08af04bd93
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-04-11 17:52:14 +0300

    Add a hook to save the access token on redirection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,16 @@
+import type { Handle } from '@sveltejs/kit';
 import { startBot } from './telegram';
 
 startBot().then(() => {
 	console.log('Bot started successfully!');
 });
+
+export const handle: Handle = async function ({ event, resolve }) {
+	const accessToken = event.url.searchParams.get('accessToken');
+	if (accessToken) {
+		event.cookies.set('accessToken', accessToken, { path: '/' });
+	}
+
+	const response = await resolve(event);
+	return response;
+};
