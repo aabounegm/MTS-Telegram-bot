5b5f1e0dc7355389a09692b14cf89d72150516fb
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-04-11 16:39:38 +0300

    Fix redirect URL

--- src/routes/charities/+layout.server.ts
+++ src/routes/charities/+layout.server.ts
@@ -1,14 +1,14 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 import { sampleCharities, type TCharity } from '$lib/api/charity';
-import { API_BASE, getAccessTokenUrl } from '$lib/api/mts';
+import { API_BASE } from '$lib/api/mts';
 
 export const load: LayoutServerLoad = async ({ fetch, cookies, url }) => {
 	let accessToken = url.searchParams.get('accessToken') ?? cookies.get('accessToken');
 	if (accessToken == undefined) {
 		// No access token found. Go to the MTS login page, and redirect to the same page again
 		// SvelteKit seems to default it to HTTP during dev
-		const redirectUrl = getAccessTokenUrl(url.href.replace('http:', 'https:'));
+		const redirectUrl = url.href.replace('http:', 'https:');
 		throw redirect(307, `/login?redirectUrl=${redirectUrl}`);
 	}
 	const res = await fetch(API_BASE + '/rnip2/charges/charitycatalog', {
@@ -19,7 +19,7 @@ export const load: LayoutServerLoad = async ({ fetch, cookies, url }) => {
 	if (!res.ok) {
 		if (res.status === 401) {
 			// Token expired. Redirect to MTS login to renew
-			const redirectUrl = getAccessTokenUrl(url.href.replace('http:', 'https:'));
+			const redirectUrl = url.href.replace('http:', 'https:');
 			throw redirect(307, `/login?redirectUrl=${redirectUrl}`);
 		}
 		// Otherwise (some other error)
