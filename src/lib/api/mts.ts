39391bc068b5f3b74e3b0dc4539d77281072cfa2
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-04-17 15:23:11 +0300

    Refactor hard-coded path into base URL variable

--- src/lib/api/mts.ts
+++ src/lib/api/mts.ts
@@ -1,10 +1,12 @@
-export const API_BASE = 'https://test.mospay.mos.ru/mospaynew';
+const BASE_URL = 'https://test.mospay.mos.ru';
+const AUTH_URL_BASE = 'https://login.mos.ru/sps/oauth/ae';
+export const API_BASE = BASE_URL + '/mospaynew';
+
 export const getAccessTokenUrl = (redirectUrl: string) => {
-	const AUTH_URL_BASE = 'https://login.mos.ru/sps/oauth/ae';
 	const params = new URLSearchParams({
 		scope: 'openid profile',
 		response_type: 'code',
-		redirect_uri: 'https://test.mospay.mos.ru/sudir/auth',
+		redirect_uri: BASE_URL + '/sudir/auth',
 		client_id: 'test.mospay.mos.ru',
 		state: redirectUrl,
 	});
