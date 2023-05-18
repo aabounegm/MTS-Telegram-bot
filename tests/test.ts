7aa1aa2d6f680d2fc24a54bf545026edb46da5d2
Marko Pezer <markopezer@gmail.com>
2023-05-18 22:43:27 +0300

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,10 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
+
+import { startBot } from './telegram';
+
+test('Bot starts successfully', async () => {
+  const result = await startBot();
+  expect(result).toBeUndefined();
+});
