48243ed964bc801ea552421b96d476534bbdc837
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-05-21 18:09:54 +0300

    Improve styles
    Co-authored-by: Виктория Губанова <56046692+GuVictory@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <style>
 	main {
 		width: calc(100% - 40px);
-		height: calc(100% - 48px);
+		min-height: 100vh;
 		padding: 24px 20px;
 
 		background-color: #f6f6f6;
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
 	import { getAccessTokenUrl } from '$lib/api/mts';
 
 	function goToLogin() {
 		const redirectUrl = $page.url.searchParams.get('redirectUrl') ?? window.location.origin;
 		const loginUrl = getAccessTokenUrl(redirectUrl);
-		goto(loginUrl);
+		window.location.href = loginUrl;
 	}
 </script>
 
@@ -14,13 +13,48 @@
 	<title>Login</title>
 </svelte:head>
 
-<p>
-	You're not logged in. To access this page, please click here to login:
-	<button on:click={goToLogin}>Login</button>
-</p>
+<div>
+	<p>You're not logged in. To access this page, please click on the button below</p>
+	<button class="button" on:click={goToLogin}>Login</button>
+</div>
 
 <style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		flex: 1;
+
+		gap: 20px;
+	}
+
 	p {
 		font-family: system-ui;
+	}
+
+	.button:hover {
+		box-shadow: 1px 2px 12px rgba(0, 0, 0, 0.25);
+	}
+
+	.button:active {
+		box-shadow: inset 2px 3px rgba(0, 0, 0, 0.25);
+	}
+
+	.button {
+		margin-top: auto;
+		width: 100%;
+		font-size: 18px;
+		font-weight: 600;
+		margin-top: 20px;
+
+		color: white;
+		background-color: #e30611;
+		padding: 10px 0;
+		border-radius: 10px;
+		text-transform: uppercase;
+		cursor: pointer;
+		transition: all 300ms ease-in-out;
+		border: none;
+		box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.25);
 	}
 </style>
