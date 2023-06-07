8a9f90525a4aa9cb31b30b032664b3a1fbe042c5
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-06-07 22:27:01 +0300

    Fix type resolution

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Sql } from 'postgres';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			sql: postgres.Sql<{}>;
+			sql: Sql;
 		}
 		// interface PageData {}
 		// interface Platform {}
