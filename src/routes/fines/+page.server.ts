2bbd31d422aff7aa3b127fe6040f00cc54777063
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-06-07 16:54:23 +0300

    Remove server-side load function for fines
    Validating Telegram data (to get chat ID) requires loading client side

--- src/routes/fines/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-import { type TFines, sampleFines } from '$lib/api/fines';
-
-export const load: PageServerLoad = async () => {
-	const fines: TFines = sampleFines;
-
-	return { fines };
-};
