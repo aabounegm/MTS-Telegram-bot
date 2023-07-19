a11057306aeb25523b79bb487d65aca672519778
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-19 18:17:04 +0300

    Factor out token check to the common layout

---
+++ src/routes/(tokenAuth)/+layout.server.ts
@@ -0,0 +1,14 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ cookies, url }) => {
+	const accessToken = url.searchParams.get('accessToken') ?? cookies.get('accessToken');
+	if (accessToken == undefined) {
+		// No access token found. Go to the MTS login page, and redirect to the same page again
+		// SvelteKit seems to default it to HTTP during dev
+		const redirectUrl = url.href.replace('http:', 'https:');
+		throw redirect(307, `/login?redirectUrl=${redirectUrl}`);
+	}
+
+	return { accessToken };
+};
